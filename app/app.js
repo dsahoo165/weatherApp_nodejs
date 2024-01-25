0149b6236eb0e0d489065631d05b6d29184be1ca
Deepak Kumar Sahoo <deepak.sahoo@conceptglobal.com>
2024-01-25 10:34:04 +0530

    test

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs')
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3000; 
 
 // Define paths for Express config  
 const publicDirectoryPath = path.join(__dirname, '/public');
